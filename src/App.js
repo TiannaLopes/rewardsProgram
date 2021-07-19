6b4f18167de159c500fee09f0f36d6834f19a016
TiannaLopes <tiannalopes@shaw.ca>
2021-07-19 08:27:11 -0400

    adjustments

---
+++ src/App.js
@@ -0,0 +1,195 @@
+import React, { useState, useEffect } from "react";
+import fetch from "./dataService";
+import ReactTable from "react-table";
+import "./App.css";
+import _ from "lodash";
+
+function calculateResults(incomingData) {
+  // Calculate points per transaction
+
+  const months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  const pointsPerTransaction = incomingData.map((transaction) => {
+    let points = 0;
+    let over100 = transaction.amt - 100;
+
+    if (over100 > 0) {
+      // A customer receives 2 points for every dollar spent over $100 in each transaction
+      points += over100 * 2;
+    }
+    if (transaction.amt > 50) {
+      // plus 1 point for every dollar spent over $50 in each transaction
+      points += 50;
+    }
+    const month = new Date(transaction.transactionDt).getMonth();
+    return { ...transaction, points, month };
+  });
+
+  let byCustomer = {};
+  let totalPointsByCustomer = {};
+  pointsPerTransaction.forEach((pointsPerTransaction) => {
+    let { id, name, month, points } = pointsPerTransaction;
+    if (!byCustomer[id]) {
+      byCustomer[id] = [];
+    }
+    if (!totalPointsByCustomer[id]) {
+      totalPointsByCustomer[name] = 0;
+    }
+    totalPointsByCustomer[name] += points;
+    if (byCustomer[id][month]) {
+      byCustomer[id][month].points += points;
+      byCustomer[id][month].monthNumber = month;
+      byCustomer[id][month].numTransactions++;
+    } else {
+      byCustomer[id][month] = {
+        id,
+        name,
+        monthNumber: month,
+        month: months[month],
+        numTransactions: 1,
+        points,
+      };
+    }
+  });
+  let tot = [];
+  for (var custKey in byCustomer) {
+    byCustomer[custKey].forEach((cRow) => {
+      tot.push(cRow);
+    });
+  }
+  let totByCustomer = [];
+  for (custKey in totalPointsByCustomer) {
+    totByCustomer.push({
+      name: custKey,
+      points: totalPointsByCustomer[custKey],
+    });
+  }
+  return {
+    total: tot,
+    pointsPerTransaction,
+    totalPointsByCustomer: totByCustomer,
+  };
+}
+
+function App() {
+  const [transactionData, setTransactionData] = useState(null);
+
+  const columns = [
+    {
+      Header: "Customer",
+      accessor: "name",
+    },
+    {
+      Header: "Month",
+      accessor: "month",
+    },
+
+    {
+      Header: "Reward Points",
+      accessor: "points",
+    },
+  ];
+  const totalsByColumns = [
+    {
+      Header: "Customer",
+      accessor: "name",
+    },
+    {
+      Header: " Reward Points",
+      accessor: "points",
+    },
+  ];
+
+  function singleTrans(row) {
+    let byCustMonth = _.filter(transactionData.pointsPerTransaction, (tRow) => {
+      return (
+        row.original.id === tRow.id &&
+        row.original.monthNumber === tRow.month
+      );
+    });
+    return byCustMonth;
+  }
+
+  useEffect(() => {
+    fetch().then((data) => {
+      const results = calculateResults(data);
+      setTransactionData(results);
+    });
+  }, []);
+
+  if (transactionData == null) {
+    return <div>Loading...</div>;
+  }
+
+  return transactionData == null ? (
+    <div>Loading...</div>
+  ) : (
+    <div>
+      <div className="container">
+        <div className="row">
+          <div className="col-10">
+            <h2>Points Rewards System Totals by Customer Months</h2>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-8">
+            <ReactTable
+              data={transactionData.total}
+              columns={columns}
+              SubComponent={(row) => {
+                return (
+                  <div>
+                    {singleTrans(row).map((tran) => {
+                      return (
+                        <div className="container">
+                          <div className="row">
+                            <div className="col-8">
+                              <strong>Transaction Date:</strong>{" "}
+                              {tran.transactionDt} - <strong>$</strong>
+                              {tran.amt} - <strong>Points: </strong>
+                              {tran.points}
+                            </div>
+                          </div>
+                        </div>
+                      );
+                    })}
+                  </div>
+                );
+              }}
+            />
+          </div>
+        </div>
+      </div>
+
+      <div className="container">
+        <div className="row">
+          <div className="col-10">
+            <h2>Points Rewards System Totals By Customer</h2>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-8">
+            <ReactTable
+              data={transactionData.totalPointsByCustomer}
+              columns={totalsByColumns}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
